d591d36180d840637956f22985ffec2b13524c21
utherp0 <ian.lawson@redhat.com>
2022-09-08 08:42:15 +0100

    First drop of Neuron Funq code

---
+++ quarkus/neuron/src/main/java/org/uth/neurons/Test1.java
@@ -0,0 +1,48 @@
+package org.uth.neurons;
+
+import io.quarkus.funqy.Context;
+import io.quarkus.funqy.Funq;
+import io.quarkus.funqy.knative.events.CloudEvent;
+import io.quarkus.funqy.knative.events.CloudEventMapping;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.subscription.UniEmitter;
+import io.vertx.core.Vertx;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+import javax.inject.Inject;
+import java.net.*;
+import java.util.*;
+
+public class Test1
+{
+  private long _start = System.currentTimeMillis();
+
+  @Inject
+  Vertx vertx;
+
+  // Use @ConfigProperty to inject ENVs here
+
+  @Funq
+  @CloudEventMapping(responseType = "neuronevent")
+  public Uni<NeuronOutput> function( Object input, @Context CloudEvent cloudEvent )
+  {
+    return Uni.createFrom().emitter( emitter ->
+    {
+      buildResponse( input, cloudEvent, emitter );
+    });
+  }
+
+  public void buildResponse( Object input, CloudEvent cloudEvent, UniEmitter<? super NeuronOutput> emitter )
+  {
+    System.out.println("Received: " + input );
+
+    // Perform Neuron processing
+    MeuronOutput output = new NeuronOutput();
+
+    output.setPayload( "{'payload','some stuff'}" );
+    emitter.complete(output);
+  }
+}
