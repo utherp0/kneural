c1dfcce2247e54f6a475a095d6b72c069d72bead
utherp0 <ian.lawson@redhat.com>
2022-09-08 12:12:14 +0100

    Moved src around to stop confusion

---
+++ neuron/src/main/java/org/uth/neurons/currency/NeuronOutput.java
@@ -0,0 +1,12 @@
+package org.uth.neurons.currency;
+
+public class NeuronOutput
+{
+  private String _payload = null;
+
+  // Setters
+  public void setPayload( String payload ) { _payload = payload; }
+
+  // Accessors
+  public String getPayload() { return _payload; }
+}
